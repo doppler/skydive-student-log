644db1c65cb1f3d6beadde1d7f34c3fcd9629dd6
David Rose <doppler@gmail.com>
2019-12-04 17:57:31 -0500

    remove unimplemented stuff from server

--- server/index.js
+++ server/index.js
@@ -1,27 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const { postgraphile } = require("postgraphile");
-const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 const path = require("path");
-
+const { postgraphile } = require("postgraphile");
 const app = express();
-
 const PORT = 4000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload());
-
-app.use(express.static(path.join(__dirname, "..", "build")));
-
-app.get("/", (req, res) => {
-  console.log("GET /");
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
-
-const video = require("./api/video");
-app.use("/api/video", video);
 
 const postgraphileOptions = {
   subscriptions: true,
@@ -48,4 +30,13 @@ const postgraphileOptions = {
 };
 
 app.use(postgraphile(process.env.DATABASE_URL, "public", postgraphileOptions));
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`api-server listening on port ${PORT}`));
+
